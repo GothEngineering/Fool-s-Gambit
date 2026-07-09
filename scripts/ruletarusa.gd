e5134840d40bdcb8e8606ecda5938312d2dd28a7
FosterLundwick <279334107+GothEngineering@users.noreply.github.com>
2026-07-09 10:22:12 -0400

    Add a lot of comments explaining the code, alongside changing some confusing function and input names
    Also  changed the game over screen slightly so It's faster

--- scripts/ruletarusa.gd
+++ scripts/ruletarusa.gd
@@ -51,8 +51,8 @@ var blood_splatters = [
 ]
 
 func _ready():
+	# Shuffle the array so the bullets are randomized
 	bullets_in_chamber.shuffle()
-	print(bullets_in_chamber)
 	bullet_used.emit(bullets_in_chamber)
 	dissapear_instructions(instructions)
 
@@ -60,15 +60,19 @@ func _process(delta: float):
 	if are_you_dead == true:
 		return
 
+	# The short spin button
 	if Input.is_action_just_pressed("LeftMouse"):
 		spin_the_cylinder()
 
+	# Debug print for own purpose only
 	if Input.is_action_just_pressed("Debug Mode"):
 		print("Current chamber: ", chamber)
 		print("Current chamber rotation: ", chamber_rotation)
 		print("The bullets in the chamber: ", bullets_in_chamber)
 		print("Current hesitation: ", hesitation)  
 
+
+	# The reloading part
 	if Input.is_action_just_pressed("R key"):
 		if pulling_the_trigger:
 			return
@@ -81,8 +85,9 @@ func _process(delta: float):
 				break
 		if is_hole_empty == false:
 			print("The revolver is full. It weighs heavily.")
+			# Change this to an actual hud element
 
-#Shooting system, try to resize this part as much as possible
+	#Shooting system
 	if Input.is_action_just_pressed("RightMouse") and not pulling_the_trigger:
 		pulling_the_trigger = true
 		await fear()
@@ -90,17 +95,19 @@ func _process(delta: float):
 		if bullets_in_chamber[chamber] > 0:
 			bullets_in_chamber[chamber] = 0
 			bang___.play()
-			deathdeathdeath()
+			game_over()
 		else:
 			empty_chamber.play()
 			rounds_counter.add_score()
 		pulling_the_trigger = false
 		spin_the_cylinder()
 
+	# The long spin button
 	if Input.is_action_just_pressed("Space bar"):
-		spin_the_cylinder(randi_range(10, 15)) #Adjust this randomizer to fit the SFX, and also buff it
+		spin_the_cylinder(randi_range(10, 15))
 
-	if Input.is_action_just_pressed("B stands for Blood"):
+	# This block enables the blood splatters on screen
+	if Input.is_action_just_pressed("Blood Button"):
 		nsfw_mode = !nsfw_mode
 		if nsfw_mode == true:
 			gore.text = """Blood Splatters: 
@@ -113,18 +120,21 @@ func _process(delta: float):
 			gore.modulate = Color.WHITE
 			blood_bank.hide()
 
+	# This rotates the cylinder upon spinning it
 	revolver_cylinder.rotation_degrees = lerp(revolver_cylinder.rotation_degrees, chamber_rotation, 0.1)
 
+	# This block causes the beginning instructions to dissapear slowly when starting the game
 func dissapear_instructions(object):
 	var instructionsfadeout = create_tween()
 	instructionsfadeout.tween_property(object, "modulate:a", 0.0, 20.0)
 	instructionsfadeout.tween_callback(object.hide)
 
+	# The spinning function
 func spin_the_cylinder(number_of_spins = 1):
 	if pulling_the_trigger:
 		return
 	chamber += number_of_spins
-	chamber = chamber % 6
+	chamber = chamber % 6 # I love you modulo
 	chamber_rotation += 60 * number_of_spins
 	bullet_used.emit(bullets_in_chamber)
 	spin_timer.start()
@@ -134,22 +144,20 @@ func spin_the_cylinder(number_of_spins = 1):
 		short_spin.play()
 		short_spin.pitch_scale = randf_range(1.0, 1.2)
 
-func deathdeathdeath():
+func game_over():
 	darkness_my_friend.show()
 	bullets_container.hide()
 	long_spin.stop()
 	are_you_dead = true
-	await get_tree().create_timer(5.0).timeout
-	grit.modulate.a = 0
-	grit.show()
+	await get_tree().create_timer(2.5).timeout
 	var animacioninsana = create_tween()
-	animacioninsana.tween_property(grit, "modulate:a", 1.0, 3.5)
 	retry_button.modulate.a = 0
 	retry_button.show()
-	animacioninsana.tween_property(retry_button, "modulate:a", 1.0, 2.0)
+	animacioninsana.tween_property(retry_button, "modulate:a", 1.0, 3.5)
 	rounds_counter.add_kill()
 	rounds_counter.reset_score()
 
+# This block causes the "delay" when pressing the trigger
 func fear():
 	hesitation += 1 
 	if hesitation < 10:
@@ -157,6 +165,7 @@ func fear():
 		hesitant_wait.start()
 		await hesitant_wait.timeout
 
+# Soft reset function so the game continues with the stats
 func keep_playing():
 	are_you_dead = false
 	darkness_my_friend.hide()
@@ -172,6 +181,7 @@ func keep_playing():
 	if nsfw_mode == true:
 		do_blood_splatters()
 
+# The "Rounds survived" shake when pressing the trigger
 func shake(target, duration = 0.2, intensity = 4.0):
 	var default_position = target.position
 	if already_shaking:
@@ -185,7 +195,7 @@ func shake(target, duration = 0.2, intensity = 4.0):
 	target.position = default_position
 	already_shaking = false
 
-
+# The blood splatter spawner
 func do_blood_splatters():
 	var yourblood = blood_splatter_scene.instantiate()
 	blood_bank.add_child(yourblood)
